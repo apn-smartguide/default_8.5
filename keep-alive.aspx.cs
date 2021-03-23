99bba824e96138da90814053cc88490c2600a7af
Brian McIntyre <brian@alphinat.com>
2021-03-23 15:45:18 -0400

    Improvments to KeepAlive

--- keep-alive.aspx.cs
+++ keep-alive.aspx.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Net;
 using System.Web;
+using System.Web.Configuration;
+using System.Web.SessionState;
 using System.Web.Security;
 
-public partial class KeepAlive : System.Web.UI.Page 
+public partial class KeepAlive : System.Web.UI.Page, IRequiresSessionState 
 {    
 
     protected void Page_Load(object sender, EventArgs e)
     {
         VerifySession();
+        //HttpContext.Current.Session["test"] = DateTime.UtcNow.ToString();
         Response.Flush();
         Response.Close();
     }	
 
     public static void VerifySession() 
     {
+        //string test = (string)HttpContext.Current.Session["test"];
         HttpCookie aspnetCookie = HttpContext.Current.Request.Cookies["ASP.NET_SessionId"];
         HttpCookie smartProfileAuthCookie = HttpContext.Current.Request.Cookies["SmartProfileAuthCookie"]; 
+
+        HttpContext.Current.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        HttpContext.Current.Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+        HttpContext.Current.Response.Cache.SetNoStore();
+        HttpContext.Current.Response.Cache.SetNoServerCaching();
 
         if (HttpContext.Current.Session != null)
         {
@@ -28,6 +37,12 @@ public partial class KeepAlive : System.Web.UI.Page
             }
             if (aspnetCookie != null && !aspnetCookie.Value.Equals(""))
             {
+                Configuration config = WebConfigurationManager.OpenWebConfiguration("~/Web.Config");
+                SessionStateSection section = (SessionStateSection)config.GetSection("system.web/sessionState");
+                int timeout = (int)section.Timeout.TotalMinutes;
+                aspnetCookie.Expires = DateTime.UtcNow.AddMinutes(timeout);
+                HttpContext.Current.Response.Cookies.Add(aspnetCookie);
+                //HttpContext.Current.Response.Write("{test:" + test + "},{session:alive},{session-id:" + aspnetCookie.Value + "},{session-expires:" + aspnetCookie.Expires + "}" + ",{timeout:" + timeout + "}");
                 HttpContext.Current.Response.Write("{session:alive}");
             }
             if (smartProfileAuthCookie != null && !smartProfileAuthCookie.Value.Equals("")) {
